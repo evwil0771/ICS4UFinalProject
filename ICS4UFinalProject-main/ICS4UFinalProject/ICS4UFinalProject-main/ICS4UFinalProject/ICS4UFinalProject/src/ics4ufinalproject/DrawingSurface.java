6d9870231c83756552ef5647f36d79623ac064c7
yuzha3124 <119621885+yuzha3124@users.noreply.github.com>
2022-12-16 14:11:22 -0500

    Add files via upload
    added fight stage

---
+++ ICS4UFinalProject-main/ICS4UFinalProject/ICS4UFinalProject-main/ICS4UFinalProject/ICS4UFinalProject/src/ics4ufinalproject/DrawingSurface.java
@@ -0,0 +1,127 @@
+/*
+ * B Cutten
+    May 2022
+    A class which allows drawing, because it extends JPanel, by way of the 
+    Graphics2D class
+    This panel is repainted regulary by implementing the Runnable interface and 
+    running in a Thread. This allows it to be animated
+ */
+package ics4ufinalproject;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class DrawingSurface extends JPanel implements MouseListener,Runnable {
+
+    private Thread animator;
+    private final int DELAY = 25;
+    // default constructor
+
+    public DrawingSurface() {
+        this.addMouseListener(this);
+        this.setFocusable(true);
+        this.requestFocus();
+    }
+
+    //does the actual drawing
+    private void doDrawing(Graphics g) {
+        //the Graphics2D class is the class that handles all the drawing
+        //must be casted from older Graphics class in order to have access to some newer methods
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setColor(new Color(255, 0, 0));//background
+        
+        
+    }
+
+    //overrides paintComponent in JPanel class
+    //performs custom painting
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);//does the necessary work to prepare the panel for drawing
+        doDrawing(g);
+    }
+
+    //update the position of the ball
+    //(we could do more complex game updates here)
+    
+    
+    
+    //this method is called after the JPanel is added to the JFrame
+    //we can perform start up tasks here
+    @Override
+    public void addNotify() {
+        super.addNotify();
+
+        animator = new Thread(this);
+        animator.start();
+    }
+
+    //this method is called only once, when the Thread starts
+    @Override
+    public void run() {
+
+        long beforeTime, timeDiff, sleep;
+        //get the current time
+        beforeTime = System.currentTimeMillis();
+
+        while (true) { //this loop runs once ever 25 ms (the DELAY)
+
+            //update the balls position
+            
+            //redraws the screen (calling the paint component method)
+            repaint();
+
+            //calculate how much time has passed since the last call
+            //this allows smooth updates and our ball will move at a constant speed (as opposed to being dependent on processor availability)
+            timeDiff = System.currentTimeMillis() - beforeTime;
+
+            //calculate how much time to wait before the next call
+            sleep = DELAY - timeDiff;
+
+            //always wait at least 2 ms
+            if (sleep < 0) {
+                sleep = 2;
+            }
+
+            //try to actually wait
+            try {
+                Thread.sleep(sleep);
+            } catch (InterruptedException e) {
+                //threads can be interrupted from other threads
+                JOptionPane.showMessageDialog(this, "Thread interrupted: " + e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+            }
+            //get the new current time
+            beforeTime = System.currentTimeMillis();
+        }
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
---
+++ ICS4UFinalProject-main/ICS4UFinalProject/ICS4UFinalProject-main/ICS4UFinalProject/ICS4UFinalProject/src/ics4ufinalproject/FightStage.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ics4ufinalproject;
+
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+
+public class FightStage extends JFrame{
+    
+    MainMenu firstWindow;
+    
+    /**
+     * creates new form second window
+     * @param m - main window
+     */
+    public FightStage(MainMenu m) {
+        //create the User interface
+        initUI();
+        firstWindow = m;
+    }
+
+    //create the custom JFrame
+    private void initUI() {        
+        //set title of the JFrame
+        setTitle("Stage");
+        //add a custom JPanel to draw on
+        add(new DrawingSurface());
+        //set the size of the window
+        setSize(300, 200);
+        //tell the JFrame what to do when closed
+        //this is important if our application has multiple windows
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+    }
+    
+}
