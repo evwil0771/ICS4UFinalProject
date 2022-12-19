f60dbd8a4482ee762a9d1d395fc361525e62516b
Braeden7933 <119624439+Braeden7933@users.noreply.github.com>
2022-12-19 13:26:36 -0500

    Update Attack.java

--- ICS4UFinalProject/ICS4UFinalProject-main/ICS4UFinalProject/ICS4UFinalProject/src/ics4ufinalproject/Attack.java
+++ ICS4UFinalProject/ICS4UFinalProject-main/ICS4UFinalProject/ICS4UFinalProject/src/ics4ufinalproject/Attack.java
@@ -4,10 +4,67 @@
  */
 package ics4ufinalproject;
 
-/**
- *
- * @author braed
- */
+import java.awt.Color;
+
 public class Attack {
     
+    private int damage;
+    private int x;
+    private int y;
+    private Color c;
+    
+    public Attack(int damage, int xPos, int yPos, Color color){
+        this.damage = damage;
+        x = xPos;
+        y = yPos;
+        c = color;
+    }
+    
+    public int getXPos(){
+        return x;
+    }
+    
+    public int getYPos(){
+        return y;
+    }
+    
+    public int getDamage(){
+        return damage;
+    }
+    
+    public void setDamage(int d){
+        damage = d;
+    }
+    
+    public Color getColor(){
+        return c;
+    }
+    
+    public void setColor(Color c){
+        this.c = c;
+    }
+    
+    public void draw(){
+        
+    }
+    
+    public void move(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+    
+    public String toString(){
+        String msg = "Damage: " + damage + "\nColour: " + c;
+        return msg;
+    }
+    
+    public Attack clone(){
+        Attack a =  new Attack(damage, x, y, c);
+        return a;
+    }
+    
+    public boolean equals(Attack a){
+        return a.getDamage() == damage;
+    }
+
 }
