0e935cd160caba0d2962830b4fdbcf923693f122
Braeden7933 <119624439+Braeden7933@users.noreply.github.com>
2022-12-19 13:02:52 -0500

    Update Attack.java

--- ICS4UFinalProject/ICS4UFinalProject-main/ICS4UFinalProject/ICS4UFinalProject/src/ics4ufinalproject/Attack.java
+++ ICS4UFinalProject/ICS4UFinalProject-main/ICS4UFinalProject/ICS4UFinalProject/src/ics4ufinalproject/Attack.java
@@ -1,6 +1,6 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * B Cross, E Wilson, Y Zhang
+ * Dec 15, 2022
  */
 package ics4ufinalproject;
 
