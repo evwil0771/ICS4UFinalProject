ee1cbc7c2d418fc316445c82b3a23b59fc5d5fb7
Braeden7933 <119624439+Braeden7933@users.noreply.github.com>
2022-12-19 14:02:19 -0500

    Update Entity.java

--- ICS4UFinalProject/ICS4UFinalProject-main/ICS4UFinalProject/ICS4UFinalProject/src/ics4ufinalproject/Entity.java
+++ ICS4UFinalProject/ICS4UFinalProject-main/ICS4UFinalProject/ICS4UFinalProject/src/ics4ufinalproject/Entity.java
@@ -6,34 +6,113 @@ package ics4ufinalproject;
 
 public interface Entity {
 
+    /**
+     * 
+     * @return 
+     */
     public String getName();
 
+    /**
+     * 
+     * @param n 
+     */
     public void setName(String n);
 
+    /**
+     * 
+     * @return 
+     */
     public int getHealth();
 
+    /**
+     * 
+     * @param h 
+     */
     public void setHealth(int h);
 
+    /**
+     * 
+     * @return 
+     */
     public int getXPos();
 
+    /**
+     * 
+     * @return 
+     */
     public int getYPos();
 
+    /**
+     * 
+     * @return 
+     */
     public int getWidth();
 
+    /**
+     * 
+     * @return 
+     */
     public int getHeight();
 
+    /**
+     * 
+     * @param h
+     * @param w 
+     */
     public void setSize(int h, int w);
 
+    /**
+     * 
+     * @param i
+     * @return 
+     */
     public Attack getAttack(int i);
 
+    /**
+     * 
+     * @param i
+     * @param a 
+     */
     public void setAttack(int i, Attack a);
 
+    /**
+     * 
+     * @param x
+     * @param y
+     * @param h
+     * @param w 
+     */
     public void draw(int x, int y, int h, int w);
 
+    /**
+     * 
+     * @param x
+     * @param y 
+     */
     public void move(int x, int y);
 
+    /**
+     * 
+     * @param a 
+     */
     public void impact(Attack a);
     
+    /**
+     * 
+     * @return 
+     */
+    public boolean isDead();
+    
+    /**
+     * 
+     * @param d 
+     */
+    public void setDead(boolean d);
+
+    /**
+     * 
+     * @return 
+     */
     public String toString();
 
 }
