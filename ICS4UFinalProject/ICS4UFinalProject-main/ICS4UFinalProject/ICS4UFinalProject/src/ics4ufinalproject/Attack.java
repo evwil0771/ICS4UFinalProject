8ec915a5341493a00883a37d211e67097fbbacb6
Braeden7933 <119624439+Braeden7933@users.noreply.github.com>
2022-12-19 14:03:02 -0500

    Update Attack.java

--- ICS4UFinalProject/ICS4UFinalProject-main/ICS4UFinalProject/ICS4UFinalProject/src/ics4ufinalproject/Attack.java
+++ ICS4UFinalProject/ICS4UFinalProject-main/ICS4UFinalProject/ICS4UFinalProject/src/ics4ufinalproject/Attack.java
@@ -13,6 +13,13 @@ public class Attack {
     private int y;
     private Color c;
     
+    /**
+     * 
+     * @param damage
+     * @param xPos
+     * @param yPos
+     * @param color 
+     */
     public Attack(int damage, int xPos, int yPos, Color color){
         this.damage = damage;
         x = xPos;
@@ -20,49 +27,94 @@ public class Attack {
         c = color;
     }
     
+    /**
+     * 
+     * @return 
+     */
     public int getXPos(){
         return x;
     }
     
+    /**
+     * 
+     * @return 
+     */
     public int getYPos(){
         return y;
     }
     
+    /**
+     * 
+     * @return 
+     */
     public int getDamage(){
         return damage;
     }
     
+    /**
+     * 
+     * @param d 
+     */
     public void setDamage(int d){
         damage = d;
     }
     
+    /**
+     * 
+     * @return 
+     */
     public Color getColor(){
         return c;
     }
     
+    /**
+     * 
+     * @param c 
+     */
     public void setColor(Color c){
         this.c = c;
     }
     
+    /**
+     * 
+     */
     public void draw(){
         
     }
     
+    /**
+     * 
+     * @param x
+     * @param y 
+     */
     public void move(int x, int y){
         this.x = x;
         this.y = y;
     }
     
+    /**
+     * 
+     * @return 
+     */
     public String toString(){
-        String msg = " Damage: " + damage + "\n Colour: " + c;
+        String msg = " Damage: " + damage + "\nColour: " + c;
         return msg;
     }
     
+    /**
+     * 
+     * @return 
+     */
     public Attack clone(){
         Attack a =  new Attack(damage, x, y, c);
         return a;
     }
     
+    /**
+     * 
+     * @param a
+     * @return 
+     */
     public boolean equals(Attack a){
         return a.getDamage() == damage;
     }
