99c2904756e936bf6ac9ea42a34d25f16c15decc
Braeden7933 <119624439+Braeden7933@users.noreply.github.com>
2022-12-20 13:32:50 -0500

    Update Entity.java

--- ICS4UFinalProject/src/ics4ufinalproject/Entity.java
+++ ICS4UFinalProject/src/ics4ufinalproject/Entity.java
@@ -1,41 +1,119 @@
 /*
  *B Cross, E Wilson, Y Zhang
  * Dec 15, 2022
+ * An interface for all entities in our program
  */
 package ics4ufinalproject;
 
 public interface Entity {
 
+    /**
+     * 
+     * @return 
+     */
     public String getName();
 
+    /**
+     * 
+     * @param n 
+     */
     public void setName(String n);
 
+    /**
+     * 
+     * @return 
+     */
     public int getHealth();
 
+    /**
+     * 
+     * @param h 
+     */
     public void setHealth(int h);
 
+    /**
+     * 
+     * @return 
+     */
     public int getXPos();
 
-    public void setXPos(int x);
-
+    /**
+     * 
+     * @return 
+     */
     public int getYPos();
 
-    public void setYPos(int y);
-
+    /**
+     * 
+     * @return 
+     */
     public int getWidth();
 
+    /**
+     * 
+     * @return 
+     */
     public int getHeight();
 
+    /**
+     * 
+     * @param h
+     * @param w 
+     */
     public void setSize(int h, int w);
 
+    /**
+     * 
+     * @param i
+     * @return 
+     */
     public Attack getAttack(int i);
 
+    /**
+     * 
+     * @param i
+     * @param a 
+     */
     public void setAttack(int i, Attack a);
 
-    abstract void draw(int x, int y, int h, int w);
+    /**
+     * 
+     * @param x
+     * @param y
+     * @param h
+     * @param w 
+     */
+    public void draw(int x, int y, int h, int w);
 
+    /**
+     * 
+     * @param x
+     * @param y 
+     */
     public void move(int x, int y);
 
+    /**
+     * 
+     * @param a 
+     */
+    public void impact(Attack a);
+    
+    /**
+     * 
+     * @return 
+     */
+    public boolean isDead();
+    
+    /**
+     * 
+     * @param d 
+     */
+    public void setDead(boolean d);
+
+    /**
+     * 
+     * @return 
+     */
     public String toString();
 
 }
