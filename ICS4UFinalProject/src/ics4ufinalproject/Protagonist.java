42bab337bc9283246b1698acfd0ddb62dca7508f
Braeden7933 <119624439+Braeden7933@users.noreply.github.com>
2022-12-21 13:13:29 -0500

    Update Protagonist.java

--- ICS4UFinalProject/src/ics4ufinalproject/Protagonist.java
+++ ICS4UFinalProject/src/ics4ufinalproject/Protagonist.java
@@ -1,8 +1,15 @@
 /*
  * B Cross, E Wilson, Y Zhang
  * Dec 15, 2022
+ * The user's playable character
  */
 package ics4ufinalproject;
+
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
 
 public class Protagonist implements Entity{
     
@@ -13,7 +20,19 @@ public class Protagonist implements Entity{
     private int height;
     private int width;
     private Attack attack;
+    private boolean dead;
+    private Image alien;
     
+    /**
+     * 
+     * @param name
+     * @param health
+     * @param posX
+     * @param posY
+     * @param height
+     * @param width
+     * @param attack 
+     */
     public Protagonist(String name, int health, int posX, int posY, int height, int width, Attack attack){
         this.name = name;
         this. health = health;
@@ -22,83 +41,174 @@ public class Protagonist implements Entity{
         this.height = height;
         this.width = width;
         this.attack = attack;
+        dead = false;
     }
     
+    /**
+     * 
+     * @return 
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * 
+     * @param n 
+     */
     public void setName(String n) {
         name = n;
     }
 
+    /**
+     * 
+     * @return 
+     */
     public int getHealth() {
         return health;
     }
 
+    /**
+     * 
+     * @param h 
+     */
     public void setHealth(int h) {
         health = h;
     }
-
+    
+    /**
+     * 
+     * @return 
+     */
     public int getXPos() {
         return x;
     }
 
-    public void setXPos(int x) {
-        this.x = x;
-    }
-
+    /**
+     * 
+     * @return 
+     */
     public int getYPos() {
         return y;
     }
 
-    public void setYPos(int y) {
-        this.y = y;
-    }
-
+    /**
+     * 
+     * @return 
+     */
     public int getWidth() {
         return width;
     }
 
+    /**
+     * 
+     * @return 
+     */
     public int getHeight() {
         return height;
     }
 
+    /**
+     * 
+     * @param h
+     * @param w 
+     */
     public void setSize(int h, int w) {
         height = h;
         width = w;
     }
 
+    /**
+     * 
+     * @param i
+     * @return 
+     */
     public Attack getAttack(int i) {
         return attack;
     }
 
+    /**
+     * 
+     * @param i
+     * @param a 
+     */
     public void setAttack(int i, Attack a) {
         attack = a;
     }
 
-    public void draw(int x, int y, int h, int w){
-        
+    /**
+     * 
+     * @param g
+     */
+    public void draw(Graphics g, JFrame m){
+        Graphics2D g2d = (Graphics2D) g;
+        alien = new ImageIcon(this.getClass().getResource("Oh_hi_mark.webp")).getImage();
+        g2d.drawImage(alien, x, y, m);
     }
 
+    /**
+     * 
+     * @param x
+     * @param y 
+     */
     public void move(int x, int y) {
         this.x = x;
         this.y = y;
     }
     
+    /**
+     * 
+     * @return 
+     */
+    public boolean isDead(){
+        return dead;
+    }
+    
+    /**
+     * 
+     * @param d 
+     */
+    public void setDead(boolean d){
+        dead = d;
+    }
+    
+    /**
+     * 
+     * @param a 
+     */
+    public void impact(Attack a){
+        health -= a.getDamage();
+        if (health <= 0){
+            dead = true;
+        }
+    }
+    
+    /**
+     * 
+     * @return 
+     */
     public String toString(){
-        String msg = "";
+        String msg = "Name: " + name + "\nHealth: " + health + "\nHeight: " + height + "\nWidth: " + width + "\nAttack: " + attack.toString();
         return msg;
     }
     
+    /**
+     * 
+     * @return 
+     */
     public Protagonist clone(){
         Protagonist p = new Protagonist(name, health, x, y, height, width, attack);
-        int i = 0;
         return p;
     }
     
+    /**
+     * 
+     * @param p
+     * @return 
+     */
     public boolean equals(Protagonist p){
         return this.name.equals(p.getName()) && this.health == p.health && this.width == p.width && this.height == p.height
                 && this.attack.equals(p.attack);
     }
 }
+
